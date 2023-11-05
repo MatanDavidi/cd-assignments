0d6ca30d6b37378ef83ae2ef6c37356ac6ffe759
Noah <nelhassanie@ethz.ch>
2023-11-05 13:08:35 +0100

    Studenttests added

--- hw3/gradedtests.ml
+++ hw3/gradedtests.ml
@@ -132,10 +132,10 @@ let gep_tests =
   ; "llprograms/gep2.ll", 4L
   ; "llprograms/gep3.ll", 1L
   ; "llprograms/gep4.ll", 2L
-  ; "llprograms/gep5.ll", 4L
+  ; "llprograms/gep5.ll", 4L 
   ; "llprograms/gep6.ll", 7L
   ; "llprograms/gep7.ll", 7L
-  ; "llprograms/gep8.ll", 2L
+  ; "llprograms/gep8.ll", 2L 
   ]
 
 let io_tests =
@@ -146,9 +146,6 @@ let io_tests =
   ; "llprograms/args1.ll", ["hello"; "compilerdesign"], "hellocompilerdesign"
   ; "llprograms/args1.ll", ["hello"; "compilerdesign"; "foo"], "argc > 3"
   ]
-
-
-
 
 (* Hidden *)
 let hidden_large_tests =
@@ -161,7 +158,125 @@ let large_tests = [ "llprograms/list1.ll", 3L
                   ; "llprograms/duplicate_factorial.ll", 240L
                   ]
 
+let prefix = "./compiler-design-eth-tests/03"
 
+let dbernhard_tests = [
+    prefix ^ "/dbernhard/load_simple.ll", 23L
+  ; prefix ^ "/dbernhard/square.ll", 144L
+  ; prefix ^ "/dbernhard/square_ptr.ll", 144L
+  ; prefix ^ "/dbernhard/return_pointer.ll", 42L
+  ; prefix ^ "/dbernhard/bitcast_1.ll", 255L
+  ; prefix ^ "/dbernhard/bitcast_2.ll", 24L
+  ; prefix ^ "/dbernhard/swap_none.ll", 42L (* performs NO swap *)
+  ; prefix ^ "/dbernhard/swap_1.ll", 10L (* swaps once*)
+  ; prefix ^ "/dbernhard/swap_2.ll", 42L (* swaps twice (same as no swap)*)
+  ; prefix ^ "/dbernhard/arguments_1.ll", 100L (* tests if all 10 arguments are taken into account *)
+  ; prefix ^ "/dbernhard/arguments_2.ll", 55L (* unique 10 arguments get summed up *)
+  ; prefix ^ "/dbernhard/arguments_3.ll", 65L (* test if the last argument is correct *)
+  ; prefix ^ "/dbernhard/update_global_value.ll", 44L (* updating of a global variable *)
+  ; prefix ^ "/dbernhard/gep_inside_struct.ll", 42L (* index inside a struct which contains an array *)
+  ; prefix ^ "/dbernhard/gep_inside_struct_array.ll", 11L (* index inside an array which is inside a struct *)
+  ; prefix ^ "/dbernhard/gep_twice.ll", 199L
+]
+
+let gep_tests = 
+  [ prefix ^ "/nicdard/gep0.ll", 1L
+  ]
+
+let local_names = 
+  [ prefix ^ "/nicdard/local_names.ll", 29L
+  ]
+
+let return_tests = 
+  [ prefix ^ "/nicdard/return42_2.ll", 42L 
+  ]
+
+(* The following tests files where already included in the project material, but they were not run in gradedtests.ml. *)
+let arithmetic_missing_tests = 
+  [ prefix ^ "/nicdard/add_twice.ll", 29L
+  ; prefix ^ "/nicdard/arith_combo.ll", 4L
+  ; prefix ^ "/nicdard/arith_combo_dce.ll", 4L
+  ; prefix ^ "/nicdard/arith_combo_fold.ll", 4L
+  ]
+
+let gep_missing_tests = 
+  [ prefix ^ "/nicdard/gep9.ll", 5L
+  ; prefix ^ "/nicdard/gep10.ll", 3L
+  ]
+
+let analysis_missing_tests = 
+  [ prefix ^ "/nicdard/analysis10_cf_opt.ll", 60L 
+  ; prefix ^ "/nicdard/analysis10_dce_opt.ll", 60L 
+  ; prefix ^ "/nicdard/analysis10.ll", 60L 
+  ; prefix ^ "/nicdard/analysis11_cf_opt.ll", 3L 
+  ; prefix ^ "/nicdard/analysis11_dce_opt.ll", 3L 
+  ; prefix ^ "/nicdard/analysis11.ll", 3L 
+  ; prefix ^ "/nicdard/analysis12_cf_opt.ll", 14L 
+  ; prefix ^ "/nicdard/analysis12_dce_opt.ll", 14L 
+  ; prefix ^ "/nicdard/analysis12.ll", 14L 
+  ; prefix ^ "/nicdard/analysis13_cf_opt.ll", 7L
+  ; prefix ^ "/nicdard/analysis13_dce_opt.ll", 7L
+  ; prefix ^ "/nicdard/analysis13.ll", 7L 
+  ; prefix ^ "/nicdard/analysis14_cf_opt.ll", 42L 
+  ; prefix ^ "/nicdard/analysis14_dce_opt.ll", 42L 
+  ; prefix ^ "/nicdard/analysis14.ll", 42L 
+  ; prefix ^ "/nicdard/analysis15_cf_opt.ll", 2L 
+  ; prefix ^ "/nicdard/analysis15_dce_opt.ll", 2L 
+  ; prefix ^ "/nicdard/analysis15.ll", 2L
+  ; prefix ^ "/nicdard/analysis16.ll", 1L
+  ; prefix ^ "/nicdard/analysis18_cf_opt.ll", 42L 
+  ; prefix ^ "/nicdard/analysis18_dce_opt.ll", 42L 
+  ; prefix ^ "/nicdard/analysis18.ll", 42L 
+  ; prefix ^ "/nicdard/analysis19_cf_opt.ll", 5L
+  ; prefix ^ "/nicdard/analysis19_dce_opt.ll", 5L
+  ; prefix ^ "/nicdard/analysis19.ll", 5L 
+  ; prefix ^ "/nicdard/analysis1_cf_opt.ll", 49L 
+  ; prefix ^ "/nicdard/analysis1.ll", 49L 
+  ; prefix ^ "/nicdard/analysis2_cf_opt.ll", 8L 
+  ; prefix ^ "/nicdard/analysis2.ll", 8L
+  ; prefix ^ "/nicdard/analysis3_cf_opt.ll", 188L 
+  ; prefix ^ "/nicdard/analysis3_dce_opt.ll", 188L 
+  ; prefix ^ "/nicdard/analysis3.ll", 188L 
+  ; prefix ^ "/nicdard/analysis4_cf_opt.ll", 254L 
+  ; prefix ^ "/nicdard/analysis4.ll", 254L 
+  ; prefix ^ "/nicdard/analysis5_cf_opt.ll", 14L 
+  ; prefix ^ "/nicdard/analysis5_dce_opt.ll", 14L 
+  ; prefix ^ "/nicdard/analysis5.ll", 14L 
+  ; prefix ^ "/nicdard/analysis6_cf_opt.ll", 2L 
+  ; prefix ^ "/nicdard/analysis6_dce_opt.ll", 2L  
+  ; prefix ^ "/nicdard/analysis7.ll", 10L 
+  ; prefix ^ "/nicdard/analysis8_cf_opt.ll", 95L 
+  ; prefix ^ "/nicdard/analysis8_dce_opt.ll", 95L 
+  ; prefix ^ "/nicdard/analysis8.ll", 95L  
+  ; prefix ^ "/nicdard/analysis9.ll", 0L
+  (* The following tests are rejected by clang due to ssa numbering order problems, 
+     but are accepted by llinterp. *)
+  ; prefix ^ "/nicdard/analysis16_cf_opt.ll", 1L
+  ; prefix ^ "/nicdard/analysis16_dce_opt.ll", 1L
+  ; prefix ^ "/nicdard/analysis1_dce_opt.ll", 49L
+  ; prefix ^ "/nicdard/analysis4_dce_opt.ll", 254L
+  ; prefix ^ "/nicdard/analysis7_cf_opt.ll", 10L
+  ; prefix ^ "/nicdard/analysis7_dce_opt.ll", 10L
+  ; prefix ^ "/nicdard/analysis9_cf_opt.ll", 0L
+  ; prefix ^ "/nicdard/analysis9_dce_opt.ll", 0L
+  (* The following tests do not run with the provided llinterp due to i1 invalid operand 
+     (althought accepted by clang). *)
+  ; prefix ^ "/nicdard/analysis2_dce_opt.ll", 8L
+  ]
+
+let zikai_tests = [
+    prefix ^ "/zikai/nested_structs.ll", 16L
+  ; prefix ^ "/zikai/nested_structs2.ll", 8L
+  ; prefix ^ "/zikai/nested_structs3.ll", 127L
+]
+
+let nicdard_tests = gep_tests
+  @ local_names
+  @ return_tests
+  @ analysis_missing_tests
+  @ gep_missing_tests
+  @ arithmetic_missing_tests  
+  
 
 let tests : suite =
   [ GradedTest("size_ty tests", 5, size_ty_tests)
@@ -175,7 +290,11 @@ let tests : suite =
   ; GradedTest("large tests", 10, executed large_tests)
   ; GradedTest("hidden large tests", 18, hidden_large_tests)
   ; GradedTest("io tests", 10, executed_io io_tests)
+  ; GradedTest("dbernhard tests", 0, executed dbernhard_tests)
+  ; GradedTest("nicdard test", 0, executed nicdard_tests)
+  ; GradedTest("zikai test", 0, executed zikai_tests)
   ]
+
 
 
 let graded_tests : suite =
